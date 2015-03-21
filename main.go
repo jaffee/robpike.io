72d22cf5b92dbed3cdb098731d1e1484b079b7f0
Matt Jaffee <jaffee@umbel.com>
2015-03-21 18:40:13 -0500

    add main

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net"
+	"time"
+	"fmt"
+)
+
+func handleConn(conn net.Conn) {
+	fmt.Printf("New Connection %v\n", conn.RemoteAddr())
+	packet := "HTTP/1.1 200 OK\nContent-Type: text/plain; charset=ascii\n\n"
+	buff := make([]byte, len(packet))
+	copy(buff[:], packet)
+	_, err := conn.Write(buff)
+	if err != nil {
+		fmt.Printf("Lost Connection %v\n", conn.RemoteAddr())
+		return
+	}
+	tb := []byte{74, 74}
+	for {
+		_, err = conn.Write(tb)
+		if err != nil {
+			fmt.Printf("Lost Connection %v\n", conn.RemoteAddr())
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":5566")
+	if err != nil {
+		return
+	}
+	for {
+		conn, err := l.Accept()
+		if err == nil {
+			go handleConn(conn)
+		}
+	}
+}
